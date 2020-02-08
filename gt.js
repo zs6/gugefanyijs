550e187dd3d5d01d02c8b33bf2c4db113579c645
zs6 <45552564+zs6@users.noreply.github.com>
2020-02-08 18:23:53 +0800

    Delete gt.js

--- gt.js
+++
@@ -1,9 +0,0 @@
-function googleTranslateElementInit() {
-            new google.translate.TranslateElement({
-            autoDisplay: true,
-            layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-            multilanguagePage: true,
-            pageLanguage: 'auto',
-            includedLanguages: 'zh-CN,zh-TW'
-            }, 'google_translate_element');
-        }
