ddae9afcac2efcc7905daf945e693798c563e2c6
zs6 <45552564+zs6@users.noreply.github.com>
2020-02-08 18:26:31 +0800

    Update element.js

--- element.js
+++ element.js
@@ -1,13 +1,4 @@
 (
-    function googleTranslateElementInit() {
-            new google.translate.TranslateElement({
-            autoDisplay: true,
-            layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-            multilanguagePage: true,
-            pageLanguage: 'auto',
-            includedLanguages: 'zh-CN,zh-TW'
-            }, 'google_translate_element');
-        }
     function() {
     var gtConstEvalStartTime = new Date();
     function d(b) {
